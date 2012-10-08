0bd1d447870709fe03f7dfdf32f0e6a9616eb9f8
dx-mon <dx-mon@users.sourceforge.net>
2012-10-08 16:49:49 +0100

    Added tests for JSONNull

---
+++ test/testJSONNull.cpp
@@ -0,0 +1,48 @@
+#include "test.h"
+
+JSONNull *testNull = NULL;
+
+void testConstruct()
+{
+	try
+	{
+		testNull = new JSONNull();
+	}
+	catch (std::bad_alloc badAlloc)
+	{
+		fail(badAlloc.what());
+	}
+	assertNotNull(testNull);
+}
+
+void testConversions()
+{
+	WANTED_TYPE(assertNull(testNull->asNull()))
+	UNWANTED_TYPE(testNull, Bool)
+	UNWANTED_TYPE(testNull, Int)
+	UNWANTED_TYPE(testNull, Float)
+	UNWANTED_TYPE(testNull, String)
+	UNWANTED_TYPE(testNull, Object)
+	UNWANTED_TYPE(testNull, Array)
+}
+
+void testDistruct()
+{
+	delete testNull;
+	testNull = NULL;
+}
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+BEGIN_REGISTER_TESTS()
+	TEST(testConstruct)
+	TEST(testConversions)
+	TEST(testDistruct)
+END_REGISTER_TESTS()
+
+#ifdef __cplusplus
+}
+#endif
