cb0fb090e11fc5246e9f469e26cb3f644566e5ad
dx-mon <dx-mon@users.sourceforge.net>
2012-10-08 17:41:02 +0100

    Added testConversions tests to testJSONString

--- test/testJSONString.cpp
+++ test/testJSONString.cpp
@@ -24,6 +24,19 @@ void testOperatorString()
 //	assertStringEqual(*testString, "testValue");
 }
 
+void testConversions()
+{
+	UNWANTED_TYPE(testString, Null)
+	UNWANTED_TYPE(testString, Bool)
+	UNWANTED_TYPE(testString, Int)
+	UNWANTED_TYPE(testString, Float)
+	// TODO: assertConstNotNull()
+	WANTED_TYPE(assertNotNull((char *)testString->asString()))
+	// WANTED_TYPE(assertStringEqual
+	UNWANTED_TYPE(testString, Object)
+	UNWANTED_TYPE(testString, Array)
+}
+
 void testDistruct()
 {
 	delete testString;
@@ -38,6 +51,7 @@ extern "C"
 BEGIN_REGISTER_TESTS()
 	TEST(testConstruct)
 	TEST(testOperatorString)
+	TEST(testConversions)
 	TEST(testDistruct)
 END_REGISTER_TESTS()
 
