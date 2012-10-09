a0a2f422d2362e4be75dbd872d6f4d0e656f38f5
dx-mon <dx-mon@users.sourceforge.net>
2012-10-09 13:07:31 +0100

    Added tests to testJSONObject for add() and []()

--- test/testJSONObject.cpp
+++ test/testJSONObject.cpp
@@ -1,6 +1,10 @@
 #include "test.h"
 
 JSONObject *testObject = NULL;
+#define KEY(name) \
+static const char *name = #name
+KEY(testKey1);
+KEY(testKey2);
 
 void testConstruct()
 {
@@ -12,10 +16,12 @@ void testConstruct()
 	{
 		fail(badAlloc.what());
 	}
+	assertNotNull(testObject);
 }
 
 void testConversions()
 {
+	assertNotNull(testObject);
 	UNWANTED_TYPE(testObject, Null)
 	UNWANTED_TYPE(testObject, Bool)
 	UNWANTED_TYPE(testObject, Int)
@@ -25,6 +31,53 @@ void testConversions()
 		assertNotNull(testObject->asObject()); \
 		assertPtrEqual(testObject->asObject(), testObject))
 	UNWANTED_TYPE(testObject, Array)
+}
+
+void testSize()
+{
+	assertNotNull(testObject);
+	assertIntEqual(testObject->size(), 0);
+}
+
+void testAdd()
+{
+	JSONAtom *child;
+	char *key;
+
+	assertNotNull(testObject);
+	assertIntEqual(testObject->size(), 0);
+
+	key = new char[strlen(testKey1) + 1];
+	strcpy(key, testKey1);
+	testObject->add(key, new JSONInt(1));
+	assertIntEqual(testObject->size(), 1);
+
+	key = new char[strlen(testKey2) + 1];
+	strcpy(key, testKey2);
+	testObject->add(key, new JSONInt(2));
+	assertIntEqual(testObject->size(), 2);
+
+	child = new JSONInt(3);
+	testObject->add((char *)testKey2, child);
+	delete child;
+
+	assertIntEqual(testObject->size(), 2);
+}
+
+void testLookup()
+{
+	JSONAtom *child;
+	assertNotNull(testObject);
+	assertIntEqual(testObject->size(), 2);
+	try
+	{
+		child = (*testObject)[testKey1];
+	}
+	catch (JSONTypeError &err)
+	{
+		fail(err.error());
+	}
+	assertIntEqual(child->asInt(), 1);
 }
 
 void testDistruct()
@@ -41,6 +94,9 @@ extern "C"
 BEGIN_REGISTER_TESTS()
 	TEST(testConstruct)
 	TEST(testConversions)
+	TEST(testSize)
+	TEST(testAdd)
+	TEST(testLookup)
 	TEST(testDistruct)
 END_REGISTER_TESTS()
 
