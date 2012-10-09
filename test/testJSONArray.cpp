814726ab991d02d2b836db0b31d65231e52ad61a
dx-mon <dx-mon@users.sourceforge.net>
2012-10-09 13:30:37 +0100

    Added tests for size(), add() and []() to testJSONArray

--- test/testJSONArray.cpp
+++ test/testJSONArray.cpp
@@ -12,10 +12,12 @@ void testConstruct()
 	{
 		fail(badAlloc.what());
 	}
+	assertNotNull(testArray);
 }
 
 void testConversions()
 {
+	assertNotNull(testArray);
 	UNWANTED_TYPE(testArray, Null)
 	UNWANTED_TYPE(testArray, Bool)
 	UNWANTED_TYPE(testArray, Int)
@@ -25,6 +27,69 @@ void testConversions()
 	WANTED_TYPE( \
 		assertNotNull(testArray->asArray()); \
 		assertPtrEqual(testArray->asArray(), testArray))
+}
+
+void testSize()
+{
+	assertNotNull(testArray);
+	assertIntEqual(testArray->size(), 0);
+}
+
+void testAdd()
+{
+	assertNotNull(testArray);
+	assertIntEqual(testArray->size(), 0);
+
+	testArray->add(new JSONInt(1));
+	assertIntEqual(testArray->size(), 1);
+
+	testArray->add(new JSONInt(2));
+	assertIntEqual(testArray->size(), 2);
+}
+
+#define COMMON_CATCH(tryStuff) \
+try \
+{ \
+	tryStuff; \
+} \
+catch (JSONTypeError &err) \
+{ \
+	fail(err.error()); \
+}
+
+#define CATCH_FAIL(tryStuff) \
+COMMON_CATCH(tryStuff) \
+catch (JSONArrayError &err) \
+{ \
+	fail(err.error()); \
+}
+
+void testLookup()
+{
+	JSONAtom *child;
+
+	assertNotNull(testArray);
+	assertIntEqual(testArray->size(), 2);
+
+	CATCH_FAIL( \
+		child = (*testArray)[0]; \
+		assertNotNull(child); \
+		assertIntEqual(child->asInt(), 1)
+	)
+
+	CATCH_FAIL( \
+		child = (*testArray)[1]; \
+		assertNotNull(child); \
+		assertIntEqual(child->asInt(), 2)
+	)
+
+	COMMON_CATCH( \
+		child = (*testArray)[2]; \
+		fail("Array index out of bounds exception not thrown when it should have been!")
+	)
+	catch (JSONArrayError &err)
+	{
+	}
 }
 
 void testDistruct()
@@ -41,6 +106,9 @@ extern "C"
 BEGIN_REGISTER_TESTS()
 	TEST(testConstruct)
 	TEST(testConversions)
+	TEST(testSize)
+	TEST(testAdd)
+	TEST(testLookup)
 	TEST(testDistruct)
 END_REGISTER_TESTS()
 
