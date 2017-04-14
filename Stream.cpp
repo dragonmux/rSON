49185f92877130be45679c79ba78e046227e27b9
DX-MON <dx-mon@users.sourceforge.net>
2017-04-14 22:11:50 +0100

    Fixed an error in the stream handling code and fixed it's lack of copyright header

--- Stream.cpp
+++ Stream.cpp
@@ -1,3 +1,21 @@
+/*
+ * This file is part of rSON
+ * Copyright © 2016-2017 Rachel Mant (dx-mon@users.sourceforge.net)
+ *
+ * rSON is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rSON is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -20,7 +38,7 @@ fileStream_t::~fileStream_t() noexcept { close(fd); }
 bool fileStream_t::read(void *const value, const size_t valueLen, size_t &actualLen)
 {
 	// If write-only and not read-write mode, or we got to eof.. return false.
-	if (eof || (mode & O_WRONLY) || !(mode & O_RDWR))
+	if (eof || ((mode & O_WRONLY) && !(mode & O_RDWR)))
 		return false;
 	ssize_t ret = ::read(fd, value, valueLen);
 	if (ret < 0)
