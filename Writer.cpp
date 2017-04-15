2f7d6cb6cf082cfc04f246370233e6b818b2f345
DX-MON <dx-mon@users.sourceforge.net>
2017-04-15 02:35:55 +0100

    Implemented a better writer for integers that works on a stream rather than snprintf() which requires a memory buffer

--- Writer.cpp
+++ Writer.cpp
@@ -19,12 +19,27 @@
 #include "internal.h"
 #include "String.h"
 #include "Memory.h"
+#include <type_traits>
+#include <utility>
 #include <string.h>
 #include <math.h>
 
 #ifdef _MSC_VER
 #define snprintf _snprintf
 #endif
+
+template<typename> struct isBoolean_ : public std::false_type { };
+template<> struct isBoolean_<bool> : public std::true_type { };
+template<typename T> struct isBoolean : public std::integral_constant<bool,
+	isBoolean_<typename std::remove_cv<T>::type>::value> { };
+
+// Type aliases for some type_traits utilities to make them easier to type and read below
+template<bool B, typename T = void> using enableIf = typename std::enable_if<B, T>::type;
+template<typename A, typename B> using isSame = std::is_same<A, B>;
+template<typename A> using isIntegral = std::is_integral<A>;
+template<typename A> using isUnsigned = std::is_unsigned<A>;
+template<typename A> using isSigned = std::is_signed<A>;
+template<typename A> using makeUnsigned = typename std::make_unsigned<A>::type;
 
 // Temporary terminal of stream store to make the compiler happy
 void JSONAtom::store(stream_t &) const { }
@@ -38,7 +53,60 @@ size_t JSONNull::length() const { return 4; }
 void JSONNull::store(stream_t &stream) const
 	{ stream.write("null", 4); }
 
-size_t JSONInt::length() const { return formatLen("%d", value); }
+template<typename int_t, typename valueType_t> struct fromInt_t
+{
+private:
+	using uint_t = makeUnsigned<int_t>;
+	const valueType_t &_value;
+
+	uint8_t calcDigits(const uint_t number) const noexcept
+	{
+		if (number < 10)
+			return 1;
+		return 1 + calcDigits(number / 10);
+	}
+
+	uint8_t digits(const int_t number) const noexcept
+	{
+		if (isSigned<int_t>::value && number < 0)
+			return 1 + calcDigits(-number);
+		return calcDigits(number);
+	}
+
+	[[gnu::noinline]] uint_t process(const uint_t number, stream_t &stream, const uint8_t digits, const size_t index) const noexcept
+	{
+		const uint_t num = number < 10 ? 0 : process(number / 10, stream, digits, index + 1) * 10;
+		stream.write(char(number - num + '0'));
+		return number;
+	}
+
+	void process(const uint_t number, stream_t &stream, const size_t index = 0) const noexcept
+		{ process(number, stream, digits(_value) - 1, index); }
+	template<typename T = int_t> enableIf<isSame<T, int_t>::value && isIntegral<T>::value && !isBoolean<T>::value && isUnsigned<T>::value>
+		format(stream_t &stream) const noexcept { process(_value, stream); }
+	template<typename T = int_t> [[gnu::noinline]] enableIf<isSame<T, int_t>::value && isIntegral<T>::value && !isBoolean<T>::value && isSigned<T>::value>
+		format(stream_t &stream) const noexcept
+	{
+		int_t number = _value;
+		if (number < 0)
+		{
+			stream.write('-');
+			process(uint_t(-number), stream, 1);
+		}
+		else
+			process(uint_t(number), stream);
+	}
+
+public:
+	constexpr fromInt_t(const valueType_t &value) noexcept : _value(value) { }
+	uint8_t length() const noexcept { return digits(_value); }
+	bool convert(stream_t &stream) const noexcept { format(stream); return stream.atEOF(); }
+};
+
+size_t JSONInt::length() const { return fromInt_t<int32_t, int32_t>(value).length(); }
+
+void JSONInt::store(stream_t &stream) const
+	{ fromInt_t<int32_t, int32_t>(value).convert(stream); }
 
 void JSONInt::store(char *str)
 {
