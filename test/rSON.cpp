d32e9c6e2468352eb32bc0db997bb9c0f7f709a4
DX-MON <dx-mon@users.sourceforge.net>
2017-04-16 12:24:41 +0100

    Begun implementing machinary for getting code coverage stats and properly testing the library header

---
+++ test/rSON.cpp
@@ -0,0 +1,27 @@
+/*
+ * This file is part of rSON
+ * Copyright © 2017 Rachel Mant (dx-mon@users.sourceforge.net)
+ *
+ * rSON is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * rSON is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// This file's mission in life is to provide code coverage
+// for the rSON header file. This implements a series of
+// wrappers for testing and must be compiled the same
+// as the library's own sources so it gets included in gcov.
+
+#include "test.h"
+#include "testHeader.h"
+
+
---
+++ test/testHeader.h
@@ -0,0 +1,6 @@
+#ifndef TEST_HEADER__H
+#define TEST_HEADER__H
+
+//
+
+#endif /*TEST_HEADER__H*/
