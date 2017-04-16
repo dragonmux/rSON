b813d893ec9dca22a814ccf394dd9b52efb90560
DX-MON <dx-mon@users.sourceforge.net>
2017-04-16 11:59:56 +0100

    Fixed the ordering of the data in the file write test as the keys in the reference data were out of order wrt the order rSON handles them in

--- test/testWriter.cpp
+++ test/testWriter.cpp
@@ -134,14 +134,13 @@ void testFileWrite()
 {
 	//TODO: Figure out what the hell to use as test reference data.
 	const char *const refData = "{\"widget\": {\"debug\": \"on\", "
-		"\"window\": {\"title\": \"Sample Konfabulator Widget\", "
-		"\"name\": \"main_window\", \"width\": 500, \"height\": 500}, "
-		"\"image\": {\"src\": \"Images/Sun.png\", \"name\": \"sun1\", "
-		"\"hOffset\": 250, \"vOffset\": 250, \"alignment\": \"center\"}, "
-		"\"text\": {\"data\": \"Click Here\", \"size\": 36, \"style\": \"bold\", "
-		"\"name\": \"text1\", \"hOffset\": 250, \"vOffset\": 100, "
-		"\"alignment\": \"center\", \"onMouseUp\": \"sun1.opacity = "
-		"(sun1.opacity / 100) * 90;\"}}}";
+		"\"image\": {\"alignment\": \"center\", \"hOffset\": 250, "
+		"\"name\": \"sun1\", \"src\": \"Images/Sun.png\", \"vOffset\": 250}, "
+		"\"text\": {\"alignment\": \"center\", \"data\": \"Click Here\", "
+		"\"hOffset\": 250, \"name\": \"text1\", \"onMouseUp\": \"sun1.opacity = "
+		"(sun1.opacity / 100) * 90;\", \"size\": 36, \"style\": \"bold\", "
+		"\"vOffset\": 100}, \"window\": {\"height\": 500, \"name\": \"main_window\", "
+		"\"title\": \"Sample Konfabulator Widget\", \"width\": 500}}}";
 	// Create the tree to write from the above blob
 	memoryStream_t sourceStream(const_cast<char *const>(refData), strlen(refData) + 1);
 	JSONAtom *json = parseJSON(sourceStream);
