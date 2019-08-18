36509d2807eb03446f72bac1fd75e93274230563
dx-mon <dx-mon@users.sourceforge.net>
2019-08-18 03:14:54 +0100

    Added an additional constraint on the installation location of crunch for the Travis-CI build to make it more predictable

--- .travis-ci/install.sh
+++ .travis-ci/install.sh
@@ -16,7 +16,7 @@ cd DX-MON/crunch
 [ "$TRAVIS_OS_NAME" == "windows" -a "$CC" == "clang" ] && EXTRA_OPTS="-Dcpp_std=c++14" || EXTRA_OPTS=""
 [ $COVERAGE -ne 0 ] && EXTRA_OPTS="$EXTRA_OPTS --buildtype=debug"
 if [ "$TRAVIS_OS_NAME" != "windows" -o "$CC" == "clang" ]; then
-	meson build --prefix=$HOME/.local $EXTRA_OPTS
+	meson build --prefix=$HOME/.local --libdir=$HOME/.local/lib $EXTRA_OPTS
 	cd build
 	ninja
 	ninja install
