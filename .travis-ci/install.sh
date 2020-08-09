afca899e6b45475ee46bae5e4eed27849514ffda
dx-mon <dx-mon@users.sourceforge.net>
2020-08-09 17:36:10 +0100

    Capped the Meson version for Travis CI while 0.55 hasn't yet been patched as this breaks the code-coverage debug builds

--- .travis-ci/install.sh
+++ .travis-ci/install.sh
@@ -5,7 +5,7 @@ set -x
 wget https://bootstrap.pypa.io/get-pip.py
 wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
 python3.6 get-pip.py --user
-pip3 install --user meson
+pip3 install --user meson~=0.54.3
 unzip ninja-linux.zip -d ~/.local/bin
 rm get-pip.py ninja-linux.zip
 
