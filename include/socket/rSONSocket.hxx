c70284ef930a589a1e1af27a6616409de9ff1793
L. E. Segovia <amy@amyspark.me>
2024-01-03 21:50:05 -0300

    socket: Use the Substrate implementation

--- include/socket/rSONSocket.hxx
+++ include/socket/rSONSocket.hxx
@@ -23,78 +23,15 @@
 #	define rSON_SOCKET_API extern rSON_SOCKET_CLS_API
 #endif
 
-#ifdef _WIN32
-#define WIN32_LEAN_AND_MEAN
-#include <ws2tcpip.h>
-#undef WIN32_LEAN_AND_MEAN
-#include <type_traits>
-
-using ssize_t = typename std::make_signed<size_t>::type;
-#endif
-
-struct sockaddr;
-struct sockaddr_storage;
+#include <substrate/socket>
 
 namespace rSON
 {
-#ifndef _WIN32
-	using socklen_t = unsigned int;
-	using sockType_t = int32_t;
-	constexpr static sockType_t INVALID_SOCKET{-1};
-#else
-	using sockType_t = SOCKET;
-#endif
-
-	struct socket_t final
-	{
-	private:
-		sockType_t socket;
-
-		bool bind(const void *const addr, const size_t len) const noexcept;
-		bool connect(const void *const addr, const size_t len) const noexcept;
-
-	public:
-		rSON_SOCKET_CLS_API constexpr socket_t() noexcept : socket(INVALID_SOCKET) { }
-		rSON_SOCKET_CLS_API constexpr socket_t(const int32_t s) noexcept : socket(s) { }
-		rSON_SOCKET_CLS_API socket_t(const int family, const int type, const int protocol = 0) noexcept;
-		socket_t(const socket_t &) = delete;
-		rSON_SOCKET_CLS_API socket_t(socket_t &&s) noexcept : socket_t() { swap(s); }
-		rSON_SOCKET_CLS_API ~socket_t() noexcept;
-
-		socket_t &operator =(const socket_t &) = delete;
-		rSON_SOCKET_CLS_API void operator =(socket_t &&s) noexcept { swap(s); }
-		rSON_SOCKET_CLS_API operator sockType_t() const noexcept { return socket; }
-		rSON_SOCKET_CLS_API bool operator ==(const sockType_t s) const noexcept { return socket == s; }
-		rSON_SOCKET_CLS_API bool operator !=(const sockType_t s) const noexcept { return socket != s; }
-		rSON_SOCKET_CLS_API void swap(socket_t &s) noexcept { std::swap(socket, s.socket); }
-		rSON_SOCKET_CLS_API bool valid() const noexcept { return socket != INVALID_SOCKET; }
-
-		template<typename T> bool bind(const T &addr) const noexcept
-			{ return bind(static_cast<const void *>(&addr), sizeof(T)); }
-		rSON_SOCKET_CLS_API bool bind(const sockaddr_storage &addr) const noexcept;
-		template<typename T> bool connect(const T &addr) const noexcept
-			{ return connect(static_cast<const void *>(&addr), sizeof(T)); }
-		rSON_SOCKET_CLS_API bool connect(const sockaddr_storage &addr) const noexcept;
-		rSON_SOCKET_CLS_API bool listen(const int32_t queueLength) const noexcept;
-		rSON_SOCKET_CLS_API socket_t accept(sockaddr *peerAddr = nullptr, socklen_t *peerAddrLen = nullptr) const noexcept;
-		rSON_SOCKET_CLS_API ssize_t write(const void *const bufferPtr, const size_t len) const noexcept;
-		rSON_SOCKET_CLS_API ssize_t read(void *const bufferPtr, const size_t len) const noexcept;
-		rSON_SOCKET_CLS_API char peek() const noexcept;
-	};
-
-	inline void swap(socket_t &a, socket_t &b) noexcept
-		{ return a.swap(b); }
-
-	enum class socketType_t : uint8_t
-	{
-		unknown,
-		ipv4,
-		ipv6,
-		dontCare
-	};
-
 	struct rpcStream_t : public stream_t
 	{
+	public:
+		using socketType_t = substrate::socketType_t;
+		using socket_t = substrate::socket_t;
 	private:
 		constexpr static const uint32_t bufferLen = 1024;
 		socketType_t family;
--- src/socket/socket.cxx
+++ src/socket/socket.cxx
@@ -5,93 +5,16 @@
 // SPDX-FileContributor: Modified by Amyspark <amy@amyspark.me>
 
 #ifndef _WIN32
-#include <sys/socket.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
+#include <sys/select.h>
 #endif
-
-#include <array>
 #include <substrate/utility>
-#include "internal/types.hxx"
 #include "rSONSocket.hxx"
 
-#ifndef _MSC_VER
-#include <unistd.h>
-inline int closesocket(const int s) { return close(s); }
-#else
-#include <Winsock2.h>
+#ifndef _WIN32
+using substrate::INVALID_SOCKET;
 #endif
-
-static inline uint16_t asIntBE(const uint16_t value) noexcept
-{
-	const std::array<uint8_t, 2> data
-	{{
-		uint8_t(value >> 8U),
-		uint8_t(value)
-	}};
-	uint16_t result{};
-	static_assert(sizeof(result) == data.size());
-	memcpy(&result, data.data(), data.size());
-	return result;
-}
-
-size_t sockaddrLen(const sockaddr_storage &addr) noexcept
-{
-	switch (addr.ss_family)
-	{
-		case AF_INET:
-			return sizeof(sockaddr_in);
-		case AF_INET6:
-			return sizeof(sockaddr_in6);
-	}
-	return sizeof(sockaddr);
-}
-
-socket_t::socket_t(const int family, const int type, const int protocol) noexcept :
-	socket(::socket(family, type, protocol)) { }
-socket_t::~socket_t() noexcept
-	{ if (socket != INVALID_SOCKET) closesocket(socket); }
-bool socket_t::bind(const void *const addr, const size_t len) const noexcept
-	{ return ::bind(socket, static_cast<const sockaddr *>(addr), len) == 0; }
-bool socket_t::bind(const sockaddr_storage &addr) const noexcept
-	{ return bind(static_cast<const void *>(&addr), sockaddrLen(addr)); }
-bool socket_t::connect(const void *const addr, const size_t len) const noexcept
-	{ return ::connect(socket, static_cast<const sockaddr *>(addr), len) == 0; }
-bool socket_t::connect(const sockaddr_storage &addr) const noexcept
-	{ return connect(static_cast<const void *>(&addr), sockaddrLen(addr)); }
-bool socket_t::listen(const int32_t queueLength) const noexcept
-	{ return ::listen(socket, queueLength) == 0; }
-socket_t socket_t::accept(sockaddr *peerAddr, socklen_t *peerAddrLen) const noexcept
-	{ return ::accept(socket, peerAddr, peerAddrLen); }
-#ifndef _MSC_VER
-ssize_t socket_t::write(const void *const bufferPtr, const size_t len) const noexcept
-	{ return ::write(socket, bufferPtr, len); }
-ssize_t socket_t::read(void *const bufferPtr, const size_t len) const noexcept
-	{ return ::read(socket, bufferPtr, len); }
-#else
-ssize_t socket_t::write(const void *const bufferPtr, const size_t len) const noexcept
-	{ return ::send(socket, static_cast<const char *const>(bufferPtr), int32_t(len), 0); }
-ssize_t socket_t::read(void *const bufferPtr, const size_t len) const noexcept
-	{ return ::recv(socket, static_cast<char *const>(bufferPtr), int32_t(len), 0); }
-#endif
-
-char socket_t::peek() const noexcept
-{
-	char buffer;
-	if (::recv(socket, &buffer, 1, MSG_PEEK) != 1)
-		return '\n';
-	return buffer;
-}
-
-int typeToFamily(const socketType_t type) noexcept
-{
-	if (type == socketType_t::ipv4)
-		return AF_INET;
-	else if (type == socketType_t::ipv6)
-		return AF_INET6;
-	return AF_UNSPEC;
-}
+using substrate::ssize_t;
+using namespace rSON;
 
 rpcStream_t::rpcStream_t() noexcept : family(socketType_t::unknown), sock(), buffer(), pos(0), lastRead(0) { }
 bool rpcStream_t::valid() const noexcept { return family != socketType_t::unknown && sock != INVALID_SOCKET; }
@@ -101,60 +24,39 @@ rpcStream_t::rpcStream_t(const socketType_t type) : family(type), sock(), buffer
 	pos(0), lastRead(0)
 {
 	if (type != socketType_t::unknown && type != socketType_t::dontCare)
-		sock = socket_t(typeToFamily(family), SOCK_STREAM, IPPROTO_TCP);
+		sock = {
+			substrate::socket::typeToFamily(family),
+			substrate::socket::protocolToType(substrate::socketProtocol_t::tcp),
+			substrate::socket::protocolToHints(substrate::socketProtocol_t::tcp)
+		};
 }
-
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
-sockaddr_storage prepare(const socketType_t family, const char *const where, const uint16_t port) noexcept
-{
-	addrinfo *results = nullptr, hints{};
-	hints.ai_family = typeToFamily(family);
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
-	hints.ai_flags = AI_PASSIVE; // This may not be right/complete..
-
-	if (getaddrinfo(where, nullptr, &hints, &results) || !results)
-		return {AF_UNSPEC};
-
-	sockaddr_storage service{};
-	memcpy(&service, results->ai_addr, sizeof(sockaddr_storage));
-	freeaddrinfo(results);
-
-	if (service.ss_family == AF_INET)
-	{
-		auto &addr = reinterpret_cast<sockaddr_in &>(service); // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast) lgtm[cpp/reinterpret-cast]
-		addr.sin_port = asIntBE(port);
-	}
-	else if (service.ss_family == AF_INET6)
-	{
-		auto &addr = reinterpret_cast<sockaddr_in6 &>(service); // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast) lgtm[cpp/reinterpret-cast]
-		addr.sin6_port = asIntBE(port);
-	}
-	else
-		return {AF_UNSPEC};
-	return service;
-}
-#pragma GCC diagnostic pop
 
 bool rpcStream_t::connect(const char *const where, const uint16_t port) noexcept
 {
-	const auto service = prepare(family, where, port);
+	const auto service = substrate::socket::prepare(family, where, port);
 	if (service.ss_family == AF_UNSPEC)
 		return false;
 	else if (family == socketType_t::dontCare)
-		sock = socket_t(service.ss_family, SOCK_STREAM, IPPROTO_TCP);
+		sock = {
+			service.ss_family,
+			substrate::socket::protocolToType(substrate::socketProtocol_t::tcp),
+			substrate::socket::protocolToHints(substrate::socketProtocol_t::tcp)
+		};
 	const_cast<rpcStream_t &>(*this).makeBuffer();
 	return sock.connect(service);
 }
 
 bool rpcStream_t::listen(const char *const where, const uint16_t port) noexcept
 {
-	const auto service = prepare(family, where, port);
+	const auto service = substrate::socket::prepare(family, where, port);
 	if (service.ss_family == AF_UNSPEC)
 		return false;
 	else if (family == socketType_t::dontCare)
-		sock = socket_t(service.ss_family, SOCK_STREAM, IPPROTO_TCP);
+		sock = {
+			service.ss_family,
+			substrate::socket::protocolToType(substrate::socketProtocol_t::tcp),
+			substrate::socket::protocolToHints(substrate::socketProtocol_t::tcp)
+		};
 	return sock.bind(service) && sock.listen(1);
 }
 
